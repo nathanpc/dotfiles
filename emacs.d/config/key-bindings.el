d6960971ef7b555618a1231771fd6a07a344823b
Nathan Campos <nathan@innoveworkshop.com>
2019-06-24 15:57:20 +0100

    Added a way to switch between C source and header files

--- emacs.d/config/key-bindings.el
+++ emacs.d/config/key-bindings.el
@@ -15,11 +15,21 @@
 ;; Smex.
 (global-set-key (kbd "M-x") 'smex)
 
-;; Better comments.
-(add-hook 'c-mode-common-hook (lambda () 
-								(local-set-key "\r"
-											   'c-doc-block-comment-return)
-								(local-set-key (kbd "/")
-											   'c-doc-block-comment-end-slash)))
+;; C/C++ mode.
+(add-hook 'c-mode-common-hook
+		  (lambda ()
+			; Toggle between C source and header files.
+			(local-set-key (kdb "C-c h") 'c-toggle-header)
+
+			; Better C comment documentation blocks.
+			(local-set-key "\r"
+						   'c-doc-block-comment-return)
+			(local-set-key (kbd "/")
+						   'c-doc-block-comment-end-slash)))
+
+;; Interpret whole buffer when in Lisp Interaction.
+(add-hook 'lisp-interaction-mode-hook
+		  (lambda ()
+			(local-set-key (kbd "C-c r") 'eval-buffer)))
 
 (provide 'key-bindings)
