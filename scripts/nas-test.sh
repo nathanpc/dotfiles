3e52efb04ba29c5534e439253204186055059224
Nathan Campos <nathan@innoveworkshop.com>
2019-10-06 14:00:27 +0100

    Scripts - Added a NAS test script

---
+++ scripts/nas-test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# nas-test.sh
+# Tests the performance of a samba share mounted in the system.
+#
+# Author: Nathan Campos <nathanpc@dreamintech.net>
+
+# Samba share mounting point.
+loc=/media/samba/Nathan
+testdir=$loc/test
+
+echo "Setting up test directory at $testdir"
+mkdir -p $testdir
+
+echo "Testing WRITE performance"
+sync
+dd if=/dev/zero of=$testdir/write.1 bs=1M count=1024 oflag=direct status=progress
+
+echo "Testing READ performance"
+sync
+dd if=$testdir/write.1 of=/dev/null bs=1M count=1024 iflag=direct status=progress
+
+echo "Cleaning up..."
+sync
+rm -r $testdir
