9d01a2d9d8b80f42d796ad204800f067f3531f03
Nathan Campos <nathan@innoveworkshop.com>
2022-06-11 13:16:05 +0100

    Scripts - Added a script to stop the newer macOS annoying prompts

---
+++ scripts/disable-macos-prompts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+### disable-macos-prompts.sh
+### Disables the annoying newer macOS prompts that I can't stand.
+###
+### Author: Nathan Campos <nathan@innoveworkshop.com>
+
+# Disable the "Are you sure you want to open this application?" dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+# Disable Disk Image Verification: Verifying ...
+defaults write com.apple.frameworks.diskimages skip-verify -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
