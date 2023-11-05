ddd0e114770628bda29ec36ce8e15b068ff3822c
Nathan Campos <nathan@innoveworkshop.com>
2023-11-05 09:38:59 +0000

    fish: Ported the configurations from my bash_profile to fish
    The fish shell now has the entire configuration that was in the
    bash_profile properly ported over. Aliases still haven't been ported
    over.

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,30 @@
+# Exposes the OS variable globally.
+set -gx OSNAME (uname -s)
+
+# Mac-specific stuff.
+if string match -q $OSNAME "Darwin"
+	# Add iTerm 2 integration.
+	test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
+	# Add MacPorts to the system PATH.
+	fish_add_path -P /opt/local/bin /opt/local/sbin
+	set -q MANPATH || set MANPATH ''
+	set -x MANPATH $MANPATH /opt/local/share/man
+end
+
+# Ensure Python packages are in our system PATH.
+if type -q python3
+	fish_add_path -P (python3 -m site --user-base)/bin
+end
+
+# Make sure our scripts, /usr/local/bin, and /sbin are in the system PATH.
+fish_add_path /usr/local/sbin /usr/local/bin /sbin /usr/local/bin
+fish_add_path -P $HOME/dotfiles/scripts
+
+# Interactive session-only stuff.
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+	# Pretty startup.
+	if type -q neofetch
+		neofetch
+	end
 end
