181055d70b48d4e202a66bf290572dc107ad07fb
Nathan Campos <nathan@innoveworkshop.com>
2024-07-11 12:02:37 +0100

    [scripts] Updated the build-timelapse.sh script

--- scripts/build-timelapse.sh
+++ scripts/build-timelapse.sh
@@ -1,27 +1,34 @@
-#/bin/sh
+#!/bin/bash
 
-### create-timelapse.sh
-### Creates timelapse videos using a bunch of images. Great if you use the
-### old GoPro's timelapse function which just gives you a bunch of images.
+### build-timelapse.sh
+### Creates a timelapse video from a sequence of GoPro image folders.
 ###
 ### Author: Nathan Campos <nathan@innoveworkshop.com>
 
-photodir=$1
-curdate=`date +"%Y%m%d%H%M%S"`
-tmplist="/tmp/timelapse-$curdate.txt"
-outfile=$2
-bitrate=10000
+# Go through directories building videos from them.
+echo "Generating a timelapse for each folder created by the camera..."
+index=0
+for dir in *; do
+	if [ -d "$dir" ]; then
+		pushd "$dir"
+		fname=`printf "../%03d.mp4" $index`
+		ffmpeg -framerate 30 -pattern_type glob -i "*.JPG" -c:v libx264 \
+			-crf 18 "$fname"
+		popd
 
-if [[ $# -lt 2 ]]; then
-	echo "Usage: $0 photodir outfile"
-	echo
-	echo -e "    photodir\tDirectory where all your JPEGs are."
-	echo -e "    outfile\tOutput file with MP4 extension."
-	echo
+		((index++))
+	fi
+done
 
-	exit 1
-fi
+# Concatenate the videos into a single one.
+echo "Concatenating videos into a continuous timelapse..."
+printf "file '%s'\n" *.mp4 > videolist.txt
+ffmpeg -f concat -i videolist.txt -c copy timelapse.mp4
+rm videolist.txt
 
-ls -1 $photodir/*.JPG > $tmplist
-mencoder -nosound -noskip -ovc lavc -lavcopts vcodec=mpeg4:vbitrate=$bitrate -o "$outfile" -mf type=jpeg:fps=30 "mf://@$tmplist"
+# Make a 1080p version for ease of sharing.
+echo "Creating a 1080p version of the timelapse..."
+ffmpeg -i timelapse.mp4 -vf 'scale=-2:1080' -c:v libx264 -crf 18 \
+	timelapse-1080p.mp4
 
+echo "Done"
